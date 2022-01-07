82b307e0da9b24c912e17d577a9700c78ee70bc7
Welington Dias <33883532+Welin7@users.noreply.github.com>
2022-01-07 01:34:32 -0300

    Using Package Refit.
    Create call of Refit pass parameter  Zipcode and print response data.

---
+++ UsingPackageRefit/UsingPackageRefit/IZipcodeApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace UsingPackageRefit
+{
+    public interface IZipcodeApiService
+    {
+        [Get("/ws/{zipcode}/json")]
+        Task<ZipcodeResponse> GetAddressAsync(string zipcode);
+    }
+}
---
+++ UsingPackageRefit/UsingPackageRefit/Program.cs
@@ -0,0 +1,28 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace UsingPackageRefit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var zipcodeClient = RestService.For<IZipcodeApiService>("http://viacep.com.br");
+                Console.WriteLine("Inform your postal code:");
+                string zipcodeInform = Console.ReadLine().ToString();
+                Console.WriteLine("consulting zip code information:" + zipcodeInform);
+
+                var response = await zipcodeClient.GetAddressAsync(zipcodeInform);
+                Console.WriteLine($"\nPublicplace:{response.Logradouro}\nComplement:{response.Complemento}\nDistrict:{response.Bairro}\nLocation:{response.Localidade}\nState:{response.Uf}\nZipCode:{response.Cep}\nIBGE:{response.Ibge}\nGIA:{response.Gia}");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error on query of Zipcode: " + e.Message);
+            }
+        }
+    }
+}
